714a0a95da9803cba4041a473d7a33cf64c78202
deibid <davidazar3@gmail.com>
2020-01-27 21:41:05 -0500

    added missing comments

--- interaction-sketch.js
+++ interaction-sketch.js
@@ -44,16 +44,16 @@ let instructionMsg = INSTRUCTIONS.GO_UP;
 
 
 
-
+//load assets before main sketch
 function preload() {
 
   QRCode = loadImage("assets/qr-code.png");
   logo = loadImage("assets/logo.svg");
   soundtrack = loadSound("assets/soundtrack.mp3");
 
-
 }
 
+//initialize and setup main objects 
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
@@ -81,19 +81,22 @@ function setup() {
     weights: 'model/model.weights.bin'
   };
 
+  //loads myo own trained data from assets folder
   neuralNetwork.load(modelFiles, posesModelLoaded);
 
+  //timer every 1 second for gameplay
   setInterval(decreaseTimer, 1000);
-
   soundtrack.loop();
 
-
 }
+
+
 
 function draw() {
   background(10);
   image(logo, 30, 10);
 
+  //if game is lost
   if (state == STATES.LOSE) {
     push();
     textAlign(CENTER, CENTER);
@@ -103,6 +106,7 @@ function draw() {
     pop();
     return;
   }
+  //if game is won
   if (state == STATES.WIN) {
     push();
     fill(255);
@@ -120,7 +124,7 @@ function draw() {
 
 
 
-
+  //if game is active
   if (state == STATES.READY) {
     push();
     //video setup, mirrored and centered
@@ -136,6 +140,7 @@ function draw() {
     //if there is a pose found...
     if (pose) {
 
+      //global flag. assign to true to see joints in body
       if (debug) {
         // draw a ball in every join
         for (let i = 0; i < pose.keypoints.length; i++) {
@@ -159,6 +164,7 @@ function draw() {
       pop();
 
 
+      //Misc UI messages
       textSize(40);
       textAlign(LEFT);
       fill(255);
@@ -176,9 +182,6 @@ function draw() {
   }
 
 
-
-
-
 }
 
 //Event called when poseNet finds a pose
@@ -190,16 +193,23 @@ function gotPoses(poses) {
     bodySkeleton = poses[0].skeleton;
 
     if (state == STATES.READY) {
+
+      //turn poseNet positions from array to what the neural net expects
       let inputs = flattenPositionArray(pose);
+
+      //classify data
       neuralNetwork.classify(inputs, classificationResult);
     }
   }
 }
 
 
+
+//Callback for when the neural network classifies the data
 function classificationResult(err, results) {
 
   if (!err) {
+    //process the result
     keepScore(results);
   } else {
     console.log(err);
@@ -208,21 +218,24 @@ function classificationResult(err, results) {
 }
 
 
-
+//determine game effects of the resulting data
 function keepScore(results) {
 
 
+  //if the pose confidence is lower than 70% don't take is as an input
   if (results[0].confidence < 0.7) {
     console.log(`low confidence ${results[0].confidence}`);
     return;
   }
 
+  //get the current pose
   let resultPose = results[0].label;
+
   //get the last pose by the player
   let lastPose = playerSequence[playerSequence.length - 1];
 
 
-  //if the new pose is different than the previous one..
+  //if the new pose is different than the previous one, trigger a change in pose
   if (lastPose != resultPose) {
 
     //get the last two poses by the player
@@ -231,7 +244,7 @@ function keepScore(results) {
 
     //if the player is coming from a down to up motion..
     if (lastTwoPoses == "ud") {
-      //if the new resulting pose is an up, increase the score.
+      //if the new resulting pose is an up, set instructions to go down
       if (resultPose == "u") {
         instructionMsg = INSTRUCTIONS.GO_DOWN;
       }
@@ -239,7 +252,7 @@ function keepScore(results) {
 
     //if the player is coming from an up to down motion..
     if (lastTwoPoses == "du") {
-      //if the new resulting pose is a down, increase the score.
+      //if the new resulting pose is a down, set instruction to go up and increase the score
       if (resultPose == "d") {
         instructionMsg = INSTRUCTIONS.GO_UP;
         gameScore++;
@@ -248,33 +261,30 @@ function keepScore(results) {
 
     //concatenate the last move to the sequence
     playerSequence += resultPose;
-    console.log(`added new pose to sequence-->\n ${playerSequence}`);
-
+    // console.log(`added new pose to sequence-->\n ${playerSequence}`);
   }
-
-  // }
-
-
 }
 
 
 
-
-
-
+//callback called every second by setInterval in setup. Keeps game play timing
 function decreaseTimer() {
 
-  console.log("Decrease timer");
-
   timer--;
+  //if time is up..
   if (timer == 0) {
 
+    //check if game was beaten
     if (gameScore < scoreToWin) {
+      //set state variable to lost. will affect draw loop
       state = STATES.LOSE;
+      //wait 5 seconds and restart game
       setTimeout(restartGame, 5000);
     }
     else {
+      //set state variable to won. will affect draw loop
       state = STATES.WIN;
+      //wait 15 seconds so users can scan QR and restart game
       setTimeout(restartGame, 15000);
     }
 
@@ -304,11 +314,13 @@ function modelLoaded() {
   console.log("ML5 model loaded. Ready to detect body");
 }
 
+//My data has been loaded. Set state to ready
 function posesModelLoaded() {
   console.log("Poses data DA loaded");
   state = STATES.READY;
 }
 
+//restart game play variables
 function restartGame() {
   state = STATES.READY;
   timer = 20;
